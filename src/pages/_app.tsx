cd7a2ec72d9f3e034a27d634f3a3185ac6b4e13c
valen <valenemartini@gmail.com>
2023-05-04 14:12:14 -0300

    quita comentario

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@component/styles/globals.css";
+import type { AppProps } from "next/app";
 import DireccionComercio from "./../components/RealizarPedido/EtapasPedido/DireccionComercio/DireccionComercio";
 import DireccionEntrega from "./../components/RealizarPedido/EtapasPedido/DireccionEntrega/DireccionEntrega";
-import { RealizarPedido } from '@component/components/RealizarPedido/RealizarPedido';
+import { RealizarPedido } from "@component/components/RealizarPedido/RealizarPedido";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-{/*       <DireccionComercio />
-      <DireccionEntrega /> */}
       <RealizarPedido />
     </div>
-
   );
 }
